9b8db2f2cec28aa5d484982b10dde96b0aab78b3
kim-Hye Soo <khwater1017@naver.com>
2021-11-24 16:13:56 +0900

    검색 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -35,7 +35,7 @@ class TestView(TestCase):
             category=self.category_culture
         )
         self.post_003 = Post.objects.create(
-            title='세번째 포스트',
+            title='세번째 파이썬 포스트',
             content='세번째 포스트',
             author=self.user_trump,
 
@@ -45,10 +45,27 @@ class TestView(TestCase):
 
         self.comment_001 =Comment.objects.create(
             post = self.post_001,
-            auther= self.user_trump,
+            author= self.user_trump,
             content= '첫번째 댓글입니다.'
 
         )
+
+    def test_search(self):
+        post_004 = Post.objects.create(
+            title="파이썬에 대한 포스트입니다.",
+            content= "Hello world...",
+            author = self.user_trump
+        )
+        response =self.client.get('/blog/search/파이썬/')
+        self.assertEqual(response.status_code,200)
+        soup= BeautifulSoup(response.content,'html.parser')
+        main_area= soup.find('div',id='main-area')
+        self.assertIn('Search : 파이썬(2)',main_area.text)
+        self.assertNotIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+
+        self.assertIn(self.post_003.title, main_area.text)
+        self.assertIn(self.post_004.title, main_area.text)
 
     def test_comment_form(self):
         self.assertEqual(Comment.objects.count(), 1)
--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [#서버 IP/blog/
 
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('<int:pk>/new_comment/',views.new_comment),
     path('update_post/<int:pk>/',views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
