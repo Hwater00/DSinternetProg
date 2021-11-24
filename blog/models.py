622f57ec75da512146a4c9fccd0c54812f998187
kim-Hye Soo <khwater1017@naver.com>
2021-11-24 16:53:01 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -70,3 +70,8 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://doitdjango.com/avatar/id/425/9afec6c6a50ba0ce/svg/{self.author.email}/'
